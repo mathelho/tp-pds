017e6482b9bf9e0ad5d2ebaa27f1c30eeed1ec3b
Felipe Guimaraes <felipeguima4@gmail.com>
2022-04-15 11:04:59 -0300

    First Commit

---
+++ banco-filmes/src/main/java/com/ufmg/pds/bancofilmes/BancoFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.ufmg.pds.bancofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFilmesApplication.class, args);
+	}
+
+}
---
+++ banco-filmes/src/test/java/com/ufmg/pds/bancofilmes/BancoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufmg.pds.bancofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
