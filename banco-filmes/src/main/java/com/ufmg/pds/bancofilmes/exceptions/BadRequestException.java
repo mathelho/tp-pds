e84b1014027987876b06a5226116724b06877a8d
Felipe Guimaraes <felipeguima4@gmail.com>
2022-06-06 19:43:58 -0300

    annotate the method

--- banco-filmes/src/main/java/com/ufmg/pds/bancofilmes/exceptions/BadRequestException.java
+++ banco-filmes/src/main/java/com/ufmg/pds/bancofilmes/exceptions/BadRequestException.java
@@ -1,5 +1,9 @@
 package com.ufmg.pds.bancofilmes.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
   public BadRequestException(String message) {
     super(message);
