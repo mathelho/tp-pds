323fb503b5308b14598dff83542036dad35d2c4c
Felipe Guimaraes <felipeguima4@gmail.com>
2022-06-16 10:30:48 -0300

    setup jacoco and create minimum tests

--- banco-filmes/src/test/java/com/ufmg/pds/bancofilmes/controllers/MovieControllerTest.java
+++ banco-filmes/src/test/java/com/ufmg/pds/bancofilmes/controllers/MovieControllerTest.java
@@ -1,21 +1,25 @@
-/*package com.ufmg.pds.bancofilmes.controllers;
+package com.ufmg.pds.bancofilmes.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ufmg.pds.bancofilmes.adapters.controllers.MovieController;
-import com.ufmg.pds.bancofilmes.services.MovieServiceWithoutDB;
+import com.ufmg.pds.bancofilmes.domains.ports.interfaces.MovieServicePort;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest
+@WebMvcTest(MovieController.class)
 class MovieControllerTest {
-  @Autowired MovieController movieController;
-  @MockBean MovieServiceWithoutDB movieService;
+
+  private static final String ENDPOINT_MOVIES = "/movies";
+  @Autowired MockMvc mockMvc;
+  @MockBean MovieServicePort movieService;
 
   @Test
-  void itShouldTest() {
-    assertEquals(1, 1);
+  void itShouldTestTheGETEndpointMoviesSuccessfully() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_MOVIES)).andExpect(status().isOk());
   }
-}*/
+}
