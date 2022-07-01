ce12d6e6e8e9729110b82d86a49044e9f905deb8
mathelho <matheus.coelho789@gmail.com>
2022-07-01 14:06:20 -0300

    Writing more test instructions

--- banco-filmes/web/cypress/e2e/banco-filmes.cy.ts
+++ banco-filmes/web/cypress/e2e/banco-filmes.cy.ts
@@ -5,5 +5,17 @@ describe('Banco de Filmes', () => {
         cy.visit('/')
 
         cy.get('[alt="Poster do filme Demon Slayer - Mugen Train: O Filme"]').click();
+
+        cy.get('button').contains('Avaliar o filme').click();
+
+        cy.get('[placeholder="Digite uma nota de 1 a 10..."]')
+            .type('9.0');
+
+        cy.get('button').contains('Enviar avaliação').click();
+
+        cy.on('window:alert', str => {
+            expect(str).to.equal('Filme avaliado!');
+        })
+
     })
 })
