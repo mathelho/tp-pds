4716024e409e77371a730f18ec8da57793f0f19f
mathelho <matheus.coelho789@gmail.com>
2022-07-02 11:54:56 -0300

    Creating second test; adding id to watch list div

--- banco-filmes/web/cypress/e2e/banco-filmes.cy.ts
+++ banco-filmes/web/cypress/e2e/banco-filmes.cy.ts
@@ -1,10 +1,11 @@
 export {}
 
 describe('Banco de Filmes', () => {
-    it('successfully loads', () => {
+    it('Successfully rates a movie', () => {
         cy.visit('/')
 
-        cy.get('[alt="Poster do filme Demon Slayer - Mugen Train: O Filme"]').click();
+        cy.get('[alt="Poster do filme Demon Slayer - Mugen Train: O Filme"]').invoke('show')
+            .click();
 
         cy.get('button').contains('Avaliar o filme').click();
 
@@ -17,5 +18,37 @@ describe('Banco de Filmes', () => {
             expect(str).to.equal('Filme avaliado!');
         })
 
+    })
+
+    it('Succesfully adds movies to watch list and removes one of them', () => {
+        cy.visit('/')
+
+        cy.get('[alt="Poster do filme Coringa"]').invoke('show')
+            .click();
+
+        cy.get('p').contains('Add').click();
+
+        cy.on('window:alert', str => {
+            expect(str).to.equal('Adicionado na Watch List!');
+        })
+
+        cy.get('button').contains('Watch List').click();
+
+        cy.contains('Coringa').should('be.visible');
+
+        cy.get('[alt="Logo do Banco de Filmes"]').click();
+
+        cy.get('[alt="Poster do filme Cidade de Deus"]').invoke('show')
+            .click();
+
+        cy.get('p').contains('Add').click();
+
+        cy.get('button').contains('Watch List').click();
+
+        cy.contains('Cidade de Deus').should('be.visible');
+
+        cy.get('svg').get('#3').children().children('rect').click({force: true});
+
+        cy.contains('Coringa').should('not.exist');
     })
 })
